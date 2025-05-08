559dd58bd0392af4bf346ff645872e35cfb49a09
GustavJones <gustavjones3@gmail.com>
2025-05-08 21:00:28 +0200

    Change structure of thread execution

--- src/Server/Server.cpp
+++ src/Server/Server.cpp
@@ -6,9 +6,7 @@
 #include <climits>
 #include <cstddef>
 #include <cstdint>
-#include <iostream>
 #include <openssl/ssl.h>
-#include <ostream>
 #include <stdexcept>
 #include <string>
 #include <thread>
@@ -32,8 +30,6 @@ void Server::Run(const std::string &_address, const uint16_t _port) {
 
 void Server::_MainLoop() {
   while (true) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
     _AcceptConnections();
     _HandleClients();
     _CloseConnections();
@@ -102,75 +98,120 @@ void Server::_Cleanup() {
 }
 
 void Server::_AcceptConnections() {
+  SSL *connection;
+
   // Poll for new connections
   if (GNetworking::SocketPoll(GetServerSocket(), GNetworkingPOLLIN)) {
-    GetClientSockets().push_back(GNetworking::SocketAccept(GetServerSocket()));
-    GLog::Log(
-        GLog::LOG_DEBUG,
-        "Opened Connection on Socket FD: " +
-            std::to_string(GetClientSockets()[GetClientSockets().size() - 1]));
+    connection = SSL_new(m_sslCTX);
+    SSL_set_fd(connection, GNetworking::SocketAccept(GetServerSocket()));
+    GetClientSockets().push_back(connection);
+    GLog::Log(GLog::LOG_DEBUG, "Opened Connection on Socket FD: " +
+                                   std::to_string(SSL_get_fd(connection)));
+    GLog::Log(GLog::LOG_DEBUG, "SSL handshake attempt: " +
+                                   std::to_string(SSL_accept(connection)));
   }
 }
 
 void Server::_HandleClients() {
-  std::vector<std::pair<std::thread, SSL *>> threadPool(m_THREAD_COUNT);
+  std::vector<std::thread> threadPool(m_THREAD_COUNT);
   size_t threadIndex = 0;
+  int output;
 
   // Poll for reading on clients
   for (size_t i = 0; i < GetClientSockets().size(); i++) {
-    if (GNetworking::SocketPoll(GetClientSockets()[i], GNetworkingPOLLIN)) {
+    if (GNetworking::SocketPoll(SSL_get_fd(GetClientSockets()[i]),
+                                GNetworkingPOLLIN)) {
       threadIndex = i % m_THREAD_COUNT;
 
       // Cleanup running thread in pool
-      if (threadPool[threadIndex].first.joinable()) {
-        threadPool[threadIndex].first.join();
-        SSL_free(threadPool[threadIndex].second);
+      if (threadPool[threadIndex].joinable()) {
+        threadPool[threadIndex].join();
       }
 
       // Create new thread
-      threadPool[threadIndex].second = SSL_new(m_sslCTX);
-      SSL_set_fd(threadPool[threadIndex].second, GetClientSockets()[i]);
-      if (SSL_accept(threadPool[threadIndex].second) <= 0) {
-        SSL_free(threadPool[threadIndex].second);
-        continue;
-      }
-
-      threadPool[threadIndex].first = std::thread(
-          &Server::_HandleOnThread, this, threadPool[threadIndex].second);
+      threadPool[threadIndex] =
+          std::thread(&Server::_HandleOnThread, this, GetClientSockets()[i]);
     }
   }
 
   for (size_t i = 0; i < m_THREAD_COUNT; i++) {
-    if (threadPool[i].first.joinable()) {
-      threadPool[i].first.join();
-
-      SSL_free(threadPool[i].second);
+    if (threadPool[i].joinable()) {
+      GLog::Log(GLog::LOG_TRACE, "Joining thread - " + std::to_string(i));
+      threadPool[i].join();
     }
   }
 }
 
 void Server::_CloseConnections() {
+  GNetworking::GNetworkingSocket sock;
+
   // Poll for closing sockets
   for (int32_t i = GetClientSockets().size() - 1; i >= 0; i--) {
-    if (GNetworking::SocketPoll(GetClientSockets()[i], GNetworkingPOLLHUP)) {
+    sock = SSL_get_fd(GetClientSockets()[i]);
+
+    if (GNetworking::SocketPoll(sock, GNetworkingPOLLHUP)) {
       GLog::Log(GLog::LOG_DEBUG,
-                "Closing Socket FD: " + std::to_string(GetClientSockets()[i]));
-      GNetworking::SocketShutdown(GetClientSockets()[i],
-                                  GNetworkingSHUTDOWNRDWR);
-      GNetworking::SocketClose(GetClientSockets()[i]);
+                "Closing Socket FD: " +
+                    std::to_string(SSL_get_fd(GetClientSockets()[i])));
+      GNetworking::SocketShutdown(sock, GNetworkingSHUTDOWNRDWR);
+      GNetworking::SocketClose(sock);
+      SSL_free(GetClientSockets()[i]);
       GetClientSockets().erase(GetClientSockets().begin() + i);
     }
   }
 }
 
 void Server::_HandleOnThread(SSL *_client) {
+  GParsing::HTTPRequest req;
+  GParsing::HTTPRequest resp;
+  constexpr size_t PEEK_INCREMENT = 512;
+  int64_t recvSize = 0;
+  int64_t recvOutput = recvSize;
+  std::vector<unsigned char> recvBuffer;
+  std::vector<unsigned char> sendBuffer;
   GNetworking::GNetworkingSocket clientSocket = SSL_get_fd(_client);
+
+  if (GNetworking::SocketPoll(clientSocket, GNetworkingPOLLIN)) {
+    while (recvOutput >= recvSize) {
+      recvSize += PEEK_INCREMENT;
+      recvBuffer.resize(recvSize);
+      m_mutex.lock();
+      recvOutput = SSL_peek(_client, recvBuffer.data(), recvSize);
+      m_mutex.unlock();
+    }
+
+    if (recvOutput <= 0) {
+      GLog::Log(GLog::LOG_WARNING, '[' + std::to_string(clientSocket) +
+                                       "]: Unable to read on socket");
+      m_mutex.lock();
+      GNetworking::SocketShutdown(clientSocket, GNetworkingSHUTDOWNRDWR);
+      m_mutex.unlock();
+      return;
+    }
+
+    recvBuffer.resize(recvOutput);
+    m_mutex.lock();
+    recvOutput = SSL_read(_client, recvBuffer.data(), recvBuffer.size());
+    m_mutex.unlock();
+
+    req.ParseRequest(recvBuffer);
+
+    GLog::Log(GLog::LOG_TRACE, (char *)recvBuffer.data());
+    resp.method = GParsing::HTTPMethod::GPARSING_GET;
+    resp.version = "HTTP/1.1";
+    resp.uri = req.uri;
+    resp.headers.push_back({"Connection", {"close"}});
+    sendBuffer = resp.CreateRequest();
+
+    m_mutex.lock();
+    SSL_write(_client, sendBuffer.data(), sendBuffer.size());
+    GNetworking::SocketShutdown(clientSocket, GNetworkingSHUTDOWNRDWR);
+    m_mutex.unlock();
+  }
 }
 
 GNetworking::GNetworkingSocket &Server::GetServerSocket() {
   return m_serverSocket;
 }
-std::vector<GNetworking::GNetworkingSocket> &Server::GetClientSockets() {
-  return m_clientSockets;
-}
+std::vector<SSL *> &Server::GetClientSockets() { return m_clientSockets; }
 } // namespace Wepp
